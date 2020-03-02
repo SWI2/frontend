05bca81e7a9c19f99760f0ebdedf3e17a1b28a8a
Joozty <32jojo32@gmail.com>
2020-03-02 16:36:42 +0100

    Project init

---
+++ .eslintrc.js
@@ -0,0 +1,56 @@
+const a11yOff = Object.keys(require('eslint-plugin-jsx-a11y').rules)
+  .reduce((acc, rule) => { acc[`jsx-a11y/${rule}`] = 'off'; return acc }, {})
+
+module.exports = {
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+
+  env: {
+    browser: true,
+  },
+
+  reportUnusedDisableDirectives: true,
+
+  rules: {
+    ...a11yOff,
+    'curly': ['error', 'multi-or-nest'],
+    'eqeqeq': ['error', 'smart'],
+    'function-call-argument-newline': ['error', 'consistent'],
+    'max-len': [
+      'error',
+      {
+        code: 80,
+        tabWidth: 2,
+        ignoreComments: false,
+        ignoreTrailingComments: false,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'no-param-reassign': ['error', { props: false }],
+    'new-parens': 'off',
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    'import/order': [
+      'error',
+      {
+        'groups': ['builtin', 'external', ['parent', 'sibling'], 'index'],
+        'newlines-between': 'always',
+      },
+    ],
+    'prettier/prettier': 'error',
+    'arrow-body-style': ['error', 'as-needed'],
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': [
+      'error',
+      {
+        skipUndeclared: true,
+        ignore: ['children', 'class', 'className', 'id', 'tag'],
+      },
+    ],
+    'react/state-in-constructor': ['error', 'never'],
+  },
+
+  plugins: ['prettier'],
+}
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# frontend
+# Frontend for fictive car service
+
+## Installation & Preview
+
+The project requires [**Node.js**][node-download] and [**NPM**][npm-install] package manager.
+
+After cloning run these commands:
+
+```sh
+npm install      # to install all dependencies
+npm run dev      # to start simple development server
+```
+
+Open [localhost:3000](http://localhost:3000) to preview.
+
+## Production deployment
+
+For using on production you need to run these commands:
+
+```sh
+npm install        # to install all dependencies
+npm run build      # to build all files 
+```
+
+Server starts listen on `localhost:3000`
+
+[node-download]: https://nodejs.org/en/download/
+[npm-install]: https://www.npmjs.com/get-npm
