f3c3d3355f530ba7ca7be88f73eb17dd03f50b0e
robertoravec141 <roravec141@gmail.com>
2020-05-24 21:47:01 +0200

    List is selectable

--- src/components/reservation-list-item/index.jsx
+++ src/components/reservation-list-item/index.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText';
+import { useHistory } from 'react-router-dom'
 
 export default function ReservationListItem(props) {
-  const { customer, rent_date, return_date } = props.reservation
+  const { id, customer, rent_date, return_date } = props.reservation
+
+  function handleClick() {
+    console.log(`Reservation id: ${id}`)
+  }
+
   return (
-    <ListItem>
+    <ListItem
+      button
+      onClick={handleClick}
+    >
         <ListItemText 
             primary={`Customer: ${customer.name}`}
             secondary={`From: ${rent_date} To: ${return_date}`}
